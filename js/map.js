e351eacba828ef6f508fbc2faa60f6504ff7f49c
Zheyuu <joreeliu1996@gmail.com>
2020-06-08 14:16:39 -0400

    fixed dynamic d3 bar

--- js/map.js
+++ js/map.js
@@ -37,34 +37,39 @@ function get_listing_geojson() {
   return jsonFeatures;
 }
 
-
 function get_neighbourhoods_geojson() {
   var request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    "http://127.0.0.1:5000/get_neighborhood_geo/",
-    true
-  );
+  request.open("GET", "http://127.0.0.1:5000/get_neighborhood_geo/", true);
 
   request.onload = function () {
     var data = JSON.parse(this.responseText);
-    geojson = L.geoJson(data,
-      {
-        onEachFeature: onEachFeature
-      }).addTo(map);
+    var nei_style = {
+      fillColor: "white",
+      weight: 1,
+      color: "black",
+      fillOpacity: 0.2,
+    };
+    geojson = L.geoJson(data, {
+      style: nei_style,
+      onEachFeature: onEachFeature,
+    }).addTo(map);
 
     // method that we will use to update the control based on feature properties passed
     info.update = function (props) {
-      this._div.innerHTML = '<h4>Location</h4>' + (props ?
-        '<b>' + props.neighbourhood + '</b><br />' + props.neighbourhood_group
-        : 'Hover over a state');
+      this._div.innerHTML =
+        "<h4>Location</h4>" +
+        (props
+          ? "<b>" +
+            props.neighbourhood +
+            "</b><br />" +
+            props.neighbourhood_group
+          : "Hover over a state");
     };
 
     info.addTo(map);
   };
   request.send();
 }
-
 
 var geojsonMarkerOptions = {
   radius: 5,
@@ -119,13 +124,12 @@ function get_neighborhood(borough) {
   request.send();
 }
 
-
 $(function () {
-  $('#select-borough').trigger('change');
-  $('#select-borough').change(function () {
+  $("#select-borough").trigger("change");
+  $("#select-borough").change(function () {
     var data = $(this).val();
 
-    if (data == 'Select Borough') {
+    if (data == "Select Borough") {
       return;
     }
 
@@ -145,11 +149,11 @@ $(function () {
 });
 
 $(function () {
-  $('#select-neighborhood').trigger('change');
-  $('#select-neighborhood').change(function () {
+  $("#select-neighborhood").trigger("change");
+  $("#select-neighborhood").change(function () {
     var data = $(this).val();
 
-    if (data == 'Select Neighborhood') {
+    if (data == "Select Neighborhood") {
       return;
     }
 
@@ -162,12 +166,14 @@ $(function () {
         break;
       }
     }
+    //get_graph(layer.feature.properties.neighbourhood);
   });
 });
 
 function highlightFeature(e) {
   layer = e.target;
   info.update(layer.feature.properties);
+  get_graph(layer.feature.properties.neighbourhood);
 }
 
 function resetHighlight(e) {
@@ -176,13 +182,13 @@ function resetHighlight(e) {
 
 function zoomToFeature(e) {
   var borough = e.target.feature.properties.neighbourhood_group;
-  $('#select-borough').val(borough);
+  $("#select-borough").val(borough);
   var request = new XMLHttpRequest();
   request.open("GET", "http://127.0.0.1:5000/get_neighborhood/", true);
   request.onload = function () {
     var neis = JSON.parse(this.responseText);
     selectNeighborhood(neis[borough]);
-    $('#select-neighborhood').val(e.target.feature.properties.neighbourhood);
+    $("#select-neighborhood").val(e.target.feature.properties.neighbourhood);
   };
   request.send();
 
@@ -193,19 +199,26 @@ function onEachFeature(feature, layer) {
   layer.on({
     mouseover: highlightFeature,
     mouseout: resetHighlight,
-    click: zoomToFeature
+    click: zoomToFeature,
   });
 }
 
 function getColor(d) {
-  return d > 1000 ? '#800026' :
-    d > 500 ? '#BD0026' :
-      d > 200 ? '#E31A1C' :
-        d > 100 ? '#FC4E2A' :
-          d > 50 ? '#FD8D3C' :
-            d > 20 ? '#FEB24C' :
-              d > 10 ? '#FED976' :
-                '#FFEDA0';
+  return d > 1000
+    ? "#800026"
+    : d > 500
+    ? "#BD0026"
+    : d > 200
+    ? "#E31A1C"
+    : d > 100
+    ? "#FC4E2A"
+    : d > 50
+    ? "#FD8D3C"
+    : d > 20
+    ? "#FEB24C"
+    : d > 10
+    ? "#FED976"
+    : "#FFEDA0";
 }
 
 function style(feature) {
@@ -213,26 +226,18 @@ function style(feature) {
     fillColor: getColor(feature.properties.density),
     weight: 2,
     opacity: 1,
-    color: 'white',
-    dashArray: '3',
-    fillOpacity: 0.7
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.7,
   };
 }
 
 function get_graph(neighborhood) {
-
   // set the dimensions and margins of the graph
-  var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 460 - margin.left - margin.right,
+  var margin = { top: 20, right: 30, bottom: 70, left: 60 },
+    width = 260 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
-
-  var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+  //console.log(width);
 
   var request = new XMLHttpRequest();
   request.open(
@@ -241,69 +246,90 @@ function get_graph(neighborhood) {
     true
   );
 
+  //console.log(svg.attr(width));
   request.onload = function () {
     var data = JSON.parse(this.responseText);
     console.log(data);
 
-    var svg = d3.select("svg"),
+    /*     var svg = d3.select("svg"),
       margin = 200,
       width = svg.attr("width") - margin,
-      height = svg.attr("height") - margin
+      height = svg.attr("height") - margin; */
+    d3.select("#barChart").selectAll("*").remove();
+    var svg = d3
+      .select("#barChart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style("stroke", "rgb(80,80,0)")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var xScale = d3.scaleBand().range([0, width]).padding(0.4),
       yScale = d3.scaleLinear().range([height, 0]);
-    var g = svg.append("g")
-      .attr("transform", "translate(" + 100 + "," + 100 + ")");
+    var g = svg.append("g");
 
-    xScale.domain(data.map(function (d) { return d.key; }));
-    yScale.domain([0, d3.max(data, function (d) { return d.val; })]);
+    xScale.domain(
+      data.map(function (d) {
+        return d.key;
+      })
+    );
+    yScale.domain([
+      0,
+      d3.max(data, function (d) {
+        return d.val;
+      }),
+    ]);
 
     g.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(xScale));
 
-    g.append("g")
-      .call(d3.axisLeft(yScale).tickFormat(function (d) {
-        return d;
-      }).ticks(10));
-
+    g.append("g").call(
+      d3
+        .axisLeft(yScale)
+        .tickFormat(function (d) {
+          return d;
+        })
+        .ticks(10)
+    );
 
     g.selectAll(".bar")
       .data(data)
-      .enter().append("rect")
+      .enter()
+      .append("rect")
       .attr("class", "bar")
-      .attr("x", function (d) { return xScale(d.key); })
-      .attr("y", function (d) { return yScale(d.val); })
+      .attr("x", function (d) {
+        return xScale(d.key);
+      })
+      .attr("y", function (d) {
+        return yScale(d.val);
+      })
       .attr("width", xScale.bandwidth())
-      .attr("height", function (d) { return height - yScale(d.val); });
-
+      .attr("height", function (d) {
+        return height - yScale(d.val);
+      });
   };
   request.send();
 }
 
-
 get_neighbourhoods_geojson();
 get_borough();
-get_graph('Midtown');
+//get_graph("Midtown");
 
-
-var map = L.map("map", { center: [40.74706, -73.84397], zoom: 13 });
+var map = L.map("map", { center: [40.742413, -73.980182], zoom: 12 });
 var geojson = L.tileLayer(
-  "http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   {
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-
   }
 ).addTo(map);
 
 var info = L.control();
 
 info.onAdd = function (map) {
-  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+  this._div = L.DomUtil.create("div", "info"); // create a div with a class "info"
   this.update();
   return this._div;
 };
-
-
-
